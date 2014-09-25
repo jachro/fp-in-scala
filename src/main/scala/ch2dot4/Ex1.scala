00739e98beff1cc78231bb0006de7cbeb8250a60
Kuba Chrobasik <jakub.chrobasik@gmail.com>
2014-09-25 22:58:31 +0100

    Exercise 1 - find n-th fibonacci number with tail recursion

--- src/main/scala/ch2dot4/Ex1.scala
+++ src/main/scala/ch2dot4/Ex1.scala
@@ -1,6 +1,44 @@
 package ch2dot4
 
-object Ex1 {
+object Fibonacci {
 
-  
+  // First number is 1
+  def findNumber(n: Int): Int = {
+    require(n > 0, "First number is 1")
+
+    @annotation.tailrec
+    def fib(counter: Int, previous: Int, acc: Int): Int = {
+      if (counter > 0)
+        fib(counter - 1, acc, acc + previous)
+      else
+        acc
+    }
+
+    if (n == 1)
+      0
+    else if (n == 2)
+      1
+    else
+      fib(n - 3, 1, 1)
+  }
+
+  // First number is 1
+  def findNumberCompletelyInLocalFunction(n: Int): Int = {
+    require(n > 0, "First number is 1")
+
+    @annotation.tailrec
+    def fib(n: Int, counter: Int, previous: Int, acc: Int): Int = {
+      if (n == 1)
+        0
+      else if (counter == 1)
+        fib(n, counter + 1, 0, 1)
+      else if (counter < n)
+        fib(n, counter + 1, acc, acc + previous)
+      else
+        acc
+    }
+
+    fib(n, 1, 0, 0)
+  }
+
 }
--- src/test/scala/ch2dot4/Ex1Spec.scala
+++ src/test/scala/ch2dot4/Ex1Spec.scala
@@ -1,5 +1,32 @@
 package ch2dot4
 
-class Ex1Spec extends org.scalatest.WordSpec {
+import org.scalatest.{Matchers, WordSpec}
 
+class Ex1Spec extends WordSpec with Matchers {
+
+  "Fibonacci number finder" should {
+    "return 0 for number 1" in {
+      Fibonacci.findNumber(1) should be(0)
+    }
+
+    "return 1 for number 2" in {
+      Fibonacci.findNumber(2) should be(1)
+    }
+
+    "return 1 for number 3" in {
+      Fibonacci.findNumber(3) should be(1)
+    }
+
+    "return 2 for number 4" in {
+      Fibonacci.findNumber(4) should be(2)
+    }
+
+    "return 34 for number 10" in {
+      Fibonacci.findNumber(10) should be(34)
+    }
+
+    "return 144 for number 13" in {
+      Fibonacci.findNumber(13) should be(144)
+    }
+  }
 }
