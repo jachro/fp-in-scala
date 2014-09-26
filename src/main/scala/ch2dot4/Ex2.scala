c8b31dfca6ccf20134df4844962e02c8b1395d07
Kuba Chrobasik <jakub.chrobasik@gmail.com>
2014-09-26 23:02:33 +0100

    Exercise 2 - implement isSorted

---
+++ src/main/scala/ch2dot4/Ex2.scala
@@ -0,0 +1,30 @@
+package ch2dot4
+
+object Ex2 {
+
+  def isSorted[A](array: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    array.foldRight[(Boolean, Option[A])]((true, None))((elem, result) => {
+      if (!result._1)
+        (false, Some(elem))
+      else if (result._2.isEmpty)
+        (true, Some(elem))
+      else
+        (ordered(elem, result._2.get), Some(elem))
+    })._1
+  }
+
+  def isSortedWithLocalFunction[A](array: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def areOrdered[A](array: Array[A], ordered: (A, A) => Boolean, idx: Int): Boolean = {
+      if (idx + 1 >= array.length)
+        true
+      else if (ordered(array(idx), array(idx + 1)))
+        areOrdered(array, ordered, idx + 1)
+      else
+        false
+    }
+
+    areOrdered(array, ordered, 0)
+  }
+
+}
---
+++ src/test/scala/ch2dot4/Ex2Spec.scala
@@ -0,0 +1,32 @@
+package ch2dot4
+
+import org.scalatest.{Matchers, WordSpec}
+
+class Ex2Spec extends WordSpec with Matchers {
+
+  "isSorted" should {
+    "say yes if given ints are in natural order" in {
+      Ex2.isSorted(Array(1, 2, 3), (n1: Int, n2: Int) => n1 <= n2) should be(true)
+    }
+
+    "say yes if given two ints are in natural order" in {
+      Ex2.isSorted(Array(2, 3), (n1: Int, n2: Int) => n1 <= n2) should be(true)
+    }
+
+    "say yes if given one element array" in {
+      Ex2.isSorted(Array(2), (n1: Int, n2: Int) => n1 <= n2) should be(true)
+    }
+
+    "say no if given ints are not in natural order" in {
+      Ex2.isSorted(Array(1, 3, 2), (n1: Int, n2: Int) => n1 <= n2) should be(false)
+    }
+
+    "say no if given two ints are in natural order" in {
+      Ex2.isSorted(Array(3, 2), (n1: Int, n2: Int) => n1 <= n2) should be(false)
+    }
+
+    "say yes if every next string in the array is longer than the previous" in {
+      Ex2.isSorted(Array("a", "aa", "aaa"), (n1: String, n2: String) => n1.length <= n2.length) should be(true)
+    }
+  }
+}
