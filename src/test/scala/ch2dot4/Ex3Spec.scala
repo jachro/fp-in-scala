154b3ccc4f7d8c794f28ec43c25adde0b4231ef5
Kuba Chrobasik <jakub.chrobasik@gmail.com>
2014-09-29 23:21:53 +0100

    Exercise 3 - currying

---
+++ src/test/scala/ch2dot4/Ex3Spec.scala
@@ -0,0 +1,16 @@
+package ch2dot4
+
+import org.scalatest.{Matchers, WordSpec}
+
+class Ex3Spec extends WordSpec with Matchers {
+
+  "curry" should {
+    "return function requiring one arg and another one" in {
+      val curried = Ex3.curry((s1: String, s2: String) => s"$s1 $s2")
+      curried("a")("b") should be ("a b")
+
+      val firstReturn = curried("d")
+      firstReturn("e") should be ("d e")
+    }
+  }
+}
