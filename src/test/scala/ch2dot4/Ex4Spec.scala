65773a024628fd0eb8833557b84298434fdfdaf7
Kuba Chrobasik <jakub.chrobasik@gmail.com>
2014-09-30 23:10:16 +0100

    Exercise 4 - uncurrying

---
+++ src/test/scala/ch2dot4/Ex4Spec.scala
@@ -0,0 +1,13 @@
+package ch2dot4
+
+import org.scalatest.{Matchers, WordSpec}
+
+class Ex4Spec extends WordSpec with Matchers {
+
+  "uncurry" should {
+    "return function requiring two args" in {
+      val uncurried = Ex4.uncurry((s: String) => (t: String) => s"$s $t")
+      uncurried("a", "b") should be ("a b")
+    }
+  }
+}
