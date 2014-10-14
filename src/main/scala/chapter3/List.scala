0cca9e10b814523e730150f6555b5a0890edc0e8
Kuba Chrobasik <jakub.chrobasik@gmail.com>
2014-10-14 23:22:14 +0100

    chapter 2; exercise 7 - product with foldRight - not finished

--- src/main/scala/chapter3/List.scala
+++ src/main/scala/chapter3/List.scala
@@ -53,4 +53,20 @@ object List {
     case Cons(h, xs) => Cons(h, init[A](xs))
     case Nil => Nil
   }
+
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = {
+    println(as)
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+  }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x,y) => x + y)
+
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)((a, b) => b match {
+    case b => println(s"$a $b");a * b
+  })
+
 }
---
+++ src/test/scala/chapter3/Ex7Spec.scala
@@ -0,0 +1,12 @@
+package chapter3
+
+import org.scalatest.{Matchers, WordSpec}
+
+class Ex7Spec extends WordSpec with Matchers {
+
+  "product2" should {
+    "be short circuit in case of 0.0 among list elements" in {
+      List.product2(List(1, 0, 3)) should be (0)
+    }
+  }
+}
